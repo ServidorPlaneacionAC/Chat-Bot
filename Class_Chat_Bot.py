4233dbe67b619ec231b80bf15ad8430bf64287ae
Santiago Marulanda V <santiagomv.96@gmail.com>
2023-12-04 09:03:04 -0500

    dd

--- Class_Chat_Bot.py
+++ Class_Chat_Bot.py
@@ -60,8 +60,10 @@ class ChatBot:
         items_ordenados = sorted(diccionario.items(), key=lambda item: item[1], reverse=True)
         top = []  
         st.write(f'{items_ordenados}')
-        if len(items_ordenados)<=3:
-            top= items_ordenados
+        if list(items_ordenados.values())[0]<=5:
+            top.append(list(items_ordenados.keys())[0])
+            top.append(list(items_ordenados.keys())[1])
+            top.append(list(items_ordenados.keys())[2])
         else:     
             for clave, valor in items_ordenados:
                 if valor != 0 and valor>10:
